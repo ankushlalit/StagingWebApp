eecff4bdfedf583cfc2fbc6760a4b167145c4363
ankushlalit <ankushlalit@gmail.com>
2023-02-27 17:09:38 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StagingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
